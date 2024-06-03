9d57b42a11e5d46e49a647cb28bfd586266ba3db
Foxie Flakey <foxieflakey@gmail.com>
2024-06-03 14:29:37 +0700

    Invite link creation

--- src/main/java/uwu/foxie/and/pawiu/java/senko_bot/Main.java
+++ src/main/java/uwu/foxie/and/pawiu/java/senko_bot/Main.java
@@ -1,8 +1,9 @@
 package uwu.foxie.and.pawiu.java.senko_bot;
 
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -12,16 +13,45 @@ import javax.security.auth.login.LoginException;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
+    private static final long BOT_PERMISSION_BITMASK = Permission.getRaw(
+        // View channel and nickname change
+        Permission.VIEW_CHANNEL,
+        Permission.NICKNAME_CHANGE,
+        
+        // Threads creations
+        Permission.CREATE_PUBLIC_THREADS,
+        Permission.CREATE_PRIVATE_THREADS,
+        
+        // Send messages
+        Permission.MESSAGE_SEND,
+        Permission.MESSAGE_SEND_IN_THREADS,
+        Permission.MESSAGE_EMBED_LINKS,
+        Permission.MESSAGE_ATTACH_FILES,
+        
+        // Read messages
+        Permission.MESSAGE_HISTORY,
+        
+        // Create polls
+        Permission.MESSAGE_SEND_POLLS
+    );
 
     public static void main(String[] args) throws LoginException {
         // get token from .env file
         Dotenv dotenv = Dotenv.load();
         String token = dotenv.get("DISCORD_TOKEN");
-
+        String clientID = dotenv.get("CLIENT_ID");
+        
         if (token == null || token.isEmpty()) {
             System.err.println("Token not found in .env file.");
             System.exit(1);
         }
+        
+        if (clientID == null || clientID.isEmpty()) {
+            System.err.println("Client ID not found in .env file.");
+            System.exit(1);
+        }
+        
+        System.out.println("Bot invite link: https://discord.com/oauth2/authorize?client_id=" + clientID + "&permission=" + Main.BOT_PERMISSION_BITMASK + "&scope=bot");
 
         JDABuilder builder = JDABuilder.createDefault(token);
         builder.enableIntents(
